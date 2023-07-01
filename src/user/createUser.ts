7a343c58174f90d060141a03d1a3938e36f2f618
Oleg Semenov <semoleg1986@gmail.com>
2023-07-01 13:04:12 +0300

    feat: add add createUser

---
+++ src/user/createUser.ts
@@ -0,0 +1,51 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { validate } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import { ErrorMessages, StatusCodes, User } from '../types';
+import { sendJsonResponse } from '../utils';
+
+function generateId(): string {
+  return uuidv4();
+}
+
+export const createUser = (
+  url: string,
+  req: IncomingMessage,
+  res: ServerResponse,
+  data: User[]
+): void => {
+  if (url === '/api/users') {
+    let body = '';
+    res.statusCode = StatusCodes.BadRequest;
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      try {
+        const newUser: User = JSON.parse(body);
+
+        if (!newUser.username || !newUser.age || !newUser.hobbies) {
+          sendJsonResponse(
+            res,
+            StatusCodes.BadRequest,
+            ErrorMessages.BadRequest
+          );
+          return;
+        }
+
+        newUser.id = validate(newUser.id) ? newUser.id : generateId();
+        data.push(newUser);
+
+        res.statusCode = StatusCodes.Created;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(newUser));
+      } catch (error) {
+        sendJsonResponse(res, StatusCodes.BadRequest, ErrorMessages.BadRequest);
+      }
+    });
+  } else {
+    sendJsonResponse(res, StatusCodes.BadRequest, ErrorMessages.IncorrectRoute);
+  }
+};

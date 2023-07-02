8289490bf89cafcc609b3b2a470128eea9600ddf
Oleg Semenov <semoleg1986@gmail.com>
2023-07-03 02:13:07 +0300

    fix: update balancer

--- src/handler/balancer.ts
+++ src/handler/balancer.ts
@@ -1,6 +1,6 @@
 import { Server } from 'http';
 import os from 'os';
-import cluster, { Worker } from 'cluster';
+import cluster from 'cluster';
 
 const cpus = os.cpus().length;
 
@@ -11,26 +11,10 @@ export const balancer = (port: number, server: Server) => {
       const workerPort = port + i + 1;
       cluster.fork({ workerPort });
     }
-    let nextWorkerIndex = 0;
-    const workers = cluster.workers as { [id: string]: Worker };
-    server.on('request', (req, res) => {
-      const worker = workers[nextWorkerIndex];
-      if (worker) {
-        worker.send({ req, res });
-        nextWorkerIndex = (nextWorkerIndex + 1) % cpus;
-      } else {
-        res.statusCode = 503;
-        res.end('No available worker to handle the request');
-      }
-    });
   } else {
     const workerPort = parseInt(process.env.workerPort || '4000');
     server.listen(workerPort, () => {
       console.log(`Worker ${process.pid} is listening on port ${workerPort}`);
-    });
-
-    process.on('message', ({ req, res }) => {
-      server.emit('request', req, res);
     });
   }
 };

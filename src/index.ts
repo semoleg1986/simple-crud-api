097d6fc81b1c860c7fad33137f7b97e0885081c2
Oleg Semenov <semoleg1986@gmail.com>
2023-07-03 02:14:14 +0300

    fix: update main

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { router } from './handler/router';
 
 import { balancer } from './handler/balancer';
 import { User } from './types';
+import cluster, { Worker } from 'cluster';
 
 const hostname: string = process.env.HOSTNAME || '';
 let port: number;
@@ -16,8 +17,18 @@ if (process.env.USE_CLUSTER) {
 
 export const data: User[] = [];
 
+const isMaster = true;
+
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   router(req, res, data);
+
+  if (process.env.USE_CLUSTER) {
+    if (cluster.isWorker) {
+      console.log(
+        `Executing request: ${req.method}. Worker ${process.pid} handles request`
+      );
+    }
+  }
 });
 
 if (process.env.USE_CLUSTER) {

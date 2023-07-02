e2562f4027f03022e2c529b1134eb8cd4246b62c
Oleg Semenov <semoleg1986@gmail.com>
2023-07-03 02:12:35 +0300

    feat: add getUser.test.ts

---
+++ src/test/getUser.test.ts
@@ -0,0 +1,96 @@
+import { ServerResponse } from 'http';
+import { getUser } from '../user/getUser';
+import { StatusCodes, User } from '../types';
+import { v4 as uuidv4 } from 'uuid';
+
+describe('getUser', () => {
+  let res: ServerResponse;
+  const mockEnd = jest.fn();
+  const mockSetHeader = jest.fn();
+
+  beforeEach(() => {
+    res = {
+      statusCode: 0,
+      setHeader: mockSetHeader,
+      end: mockEnd
+    } as unknown as ServerResponse;
+    mockSetHeader.mockClear();
+    mockEnd.mockClear();
+  });
+
+  it('should return all users', () => {
+    const data: User[] = [
+      {
+        id: uuidv4(),
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      },
+      {
+        id: uuidv4(),
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      }
+    ];
+    getUser('/api/users', res, data);
+    expect(res.statusCode).toBe(StatusCodes.OK);
+    expect(mockSetHeader).toHaveBeenCalledWith(
+      'Content-Type',
+      'application/json'
+    );
+    expect(mockEnd).toHaveBeenCalledWith(JSON.stringify(data));
+  });
+  it('should return 404 if user ID is not found', () => {
+    const data: User[] = [
+      {
+        id: uuidv4(),
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      },
+      {
+        id: uuidv4(),
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      }
+    ];
+    const userUUID = uuidv4();
+    getUser(`/api/users/${userUUID}`, res, data);
+    expect(res.statusCode).toBe(StatusCodes.NotFound);
+    expect(mockSetHeader).toHaveBeenCalledWith(
+      'Content-Type',
+      'application/json'
+    );
+    expect(mockEnd).toHaveBeenCalledWith(
+      JSON.stringify({ error: 'User not found' })
+    );
+  });
+  it('should return 400 if invalid ID is provided', () => {
+    const data: User[] = [
+      {
+        id: 'd49aa6c2-e438-4952-8864-9b8dc40d800a',
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      },
+      {
+        id: 'd49aa6c2-e438-4952-8864-9b8dc40d800a',
+        username: 'John',
+        age: 25,
+        hobbies: ['reading', 'running']
+      }
+    ];
+    const userUUID = uuidv4();
+    getUser(`/api/users/d49aa6c2-e438-4952-8864-9b8dc40d800123a`, res, data);
+    expect(res.statusCode).toBe(StatusCodes.BadRequest);
+    expect(mockSetHeader).toHaveBeenCalledWith(
+      'Content-Type',
+      'application/json'
+    );
+    expect(mockEnd).toHaveBeenCalledWith(
+      JSON.stringify({ error: 'Invalid user ID' })
+    );
+  });
+});
